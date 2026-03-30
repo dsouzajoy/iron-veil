4f26339993eb5b947c42c01d092e048c2a8d9c4b
joydesouza <joy.dsouza@smallcase.com>
2026-03-31 00:54:41 +0530

    ci: add workflow to build and deploy on gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   resolve: {
     // Allow '@/foo' imports from anywhere in src/
     alias: { '@': '/src' },
